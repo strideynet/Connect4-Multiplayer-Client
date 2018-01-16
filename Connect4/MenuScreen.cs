78c2ee97f03df55959a0b1fb4638a64a0ea585ff
Unknown <noah.stride@selfsown.com>
2018-01-16 22:09:34 +0000

    HM

--- Connect4/MenuScreen.cs
+++ Connect4/MenuScreen.cs
@@ -15,7 +15,7 @@ namespace Connect4 {
         }
 
         private void btnMatchmake_Click(object sender, EventArgs e) {
-            MultiplayerConnection multiplayerConnection = new MultiplayerConnection("ws://127.0.0.1:80/", txtUsername.Text, this); //lwm server: 91.134.107.74
+            MultiplayerConnection multiplayerConnection = new MultiplayerConnection("ws://91.134.107.74:80/", txtUsername.Text, this); //lwm server: 91.134.107.74
         }
     }
 }
